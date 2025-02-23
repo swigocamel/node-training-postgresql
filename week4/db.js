5b4e67abe1f0aff24ac552e2ffa8e4da943963d1
swigocamel <swigocamel@gmail.com>
2025-02-23 13:46:54 +0800

    update table defination

--- week4/db.js
+++ week4/db.js
@@ -10,6 +10,27 @@ const CreditPackage = new EntitySchema({
       generated: "uuid",
       nullable: false,
     },
+    name: {
+      type: "varchar",
+      length: 50,
+      nullable: false,
+      unique: true,
+    }
+    credit_amount: {
+      type: "integer",
+      nullable: false,
+    }
+    price: {
+      type: "numeric",
+      precision: 10,
+      scale: 2,
+      nullable: false,
+    }
+    created_at: {
+      type: "timestamp",
+      nullable: false,
+      createDate: true,
+    }
   },
 })
 

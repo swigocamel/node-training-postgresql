77abafc2d5f95ac134b6cf9ef1a4c52fbf2e21f7
swigocamel <swigocamel@gmail.com>
2025-03-01 15:23:48 +0800

    add GET credit package

--- week5/routes/creditPackage.js
+++ week5/routes/creditPackage.js
@@ -5,7 +5,17 @@ const { dataSource } = require('../db/data-source')
 const logger = require('../utils/logger')('CreditPackage')
 
 router.get('/', async (req, res, next) => {
-
+    try {
+        const data = await dataSource.getRepository("CreditPackage").find({
+          select: ['id', 'name', 'credit_amount', 'price']
+        })
+        res.status(200).json({
+          status: "success",
+          data: data,
+        })
+      } catch (error) {
+        next(error)
+    }  
 })
 
 router.post('/', async (req, res, next) => {

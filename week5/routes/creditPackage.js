26111aa6f2f004a898ae6a0996ef72ea185df43d
swigocamel <swigocamel@gmail.com>
2025-03-01 15:30:40 +0800

    add POST credit package

--- week5/routes/creditPackage.js
+++ week5/routes/creditPackage.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const { dataSource } = require('../db/data-source')
 const logger = require('../utils/logger')('CreditPackage')
+const { isValidString, isNumber } = require('../utils/validator')
 
 router.get('/', async (req, res, next) => {
     try {
@@ -19,6 +20,45 @@ router.get('/', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
+    try {
+        const { name, credit_amount, price } = req.body
+        if (!isValidString(name) || !isNumber(credit_amount) || !isNumber(price)) {
+          res.status(400).json({
+            status: "failed",
+            message: "欄位未正確填寫",
+          })
+          return
+        }
+
+        const CreditPackage = dataSource.getRepository("CreditPackage");
+        const findCreditPackage = await CreditPackage.find({
+          where: { 
+            name: name
+          } 
+        })
+        if (findCreditPackage.length > 0) {
+          res.status(409).json({
+            status: "failed",
+            message: "資料重複",
+          })
+          return
+        }
+
+        const newCreditPackage = CreditPackage.create({
+          name,
+          credit_amount,
+          price
+        })
+
+        const result = await CreditPackage.save(newCreditPackage);
+
+        res.status(200).json({
+          status: "success",
+          data: result,
+        })
+      } catch (error) {
+        next(error)
+    }
 })
 
 router.delete('/:creditPackageId', async (req, res, next) => {

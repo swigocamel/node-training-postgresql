738946770facc49fa162d24a792f50850f606d78
swigocamel <swigocamel@gmail.com>
2025-03-16 11:40:45 +0800

    verify JWT token

--- week6/utils/jwtUtils.js
+++ week6/utils/jwtUtils.js
@@ -11,6 +11,26 @@ const generateJWT = (payload)=> {
   );
 }
 
+const verifyJWT = (token) => {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, config.get('secret.jwtSecret'), (err, decoded) => {
+        if (err) {
+            switch (err.name) {
+                case 'TokenExpiredError':
+                  reject(appError(401, 'Token 已過期'))
+                  break
+                default:
+                  reject(appError(401, '無效的 token'))
+                  break
+              }
+        } else {
+          resolve(decoded)
+        }
+      })
+    })
+  }
+
 module.exports = { 
   generateJWT,
+  verifyJWT
 };
